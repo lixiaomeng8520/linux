b02e2c7bc9cd3dac7ceff2447979795b9286cd8c
lixiaomeng <lixiaomeng8520@163.com>
2016-09-09 15:02:53 +0800

    common

--- common.sh
+++ common.sh
@@ -14,4 +14,15 @@ iptables -P OUTPUT DROP
 #删除.列出规则编号后，可以根据编号进行删除
 iptables -D INPUT 2
 #保存
-service iptables save
+service iptables save # centos
+#开放环回接口
+iptables -A INPUT -i lo -j ACCEPT
+#允许已建立的连接接收数据
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+#允许80端口新建立数据
+iptables -A INPUT -p tcp --dport 80 -m state --state NEW -j ACCEPT
+#脚本
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT ACCEPT
+iptables -A INPUT -i lo -j ACCEPT
